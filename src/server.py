9a63c3252d37806da1d32a0c67166e96a8ca92b0
Kit Glover <kglover@live.com.au>
2022-01-22 19:24:20 +1100

    Added new player and active game routes to server

--- src/server.py
+++ src/server.py
@@ -1,8 +1,13 @@
+from crypt import methods
 from json import dumps
 from flask import Flask, request
 from chess import WHITE, BLACK
 
 most_recent_move = {'player': BLACK, 'move': [(1, 1), (2, 1)]}
+
+assigned_white = False
+assigned_black = False
+game_active = False
 
 APP = Flask(__name__)
 
@@ -20,7 +25,6 @@ def validPost(data):
 def get_most_recent():
     return dumps(most_recent_move)
 
-
 @APP.route('/move', methods=['POST'])
 def post_new_move():
     data = request.get_json()
@@ -28,6 +32,25 @@ def post_new_move():
     validPost(data)
     most_recent_move = data
     return {}
+
+@APP.route('/player/new', methods=['PUT'])
+def new_player():
+    global assigned_white
+    global assigned_black
+    if not assigned_white:
+        assigned_white = True
+        return dumps(WHITE)
+    if not assigned_black:
+        global game_active
+        game_active = True
+        assigned_black = True
+        return dumps(BLACK)
+    else:
+        raise ValueError("Already two players playing")
+
+@APP.route('/game/active', methods=['GET'])
+def is_game_active():
+    return dumps(game_active)
 
 if __name__ == '__main__':
     APP.run()
