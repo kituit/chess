4ce433a23ae78972a526400445a930d5b29bcebc
Kit Glover <kglover@live.com.au>
2022-01-28 20:07:37 +1100

    Updated compile script

--- compile_gui.sh
+++ compile_gui.sh
@@ -1,3 +1,3 @@
 # Compiles program at dist/gui, run script bash compile_gui.sh
-pyinstaller --add-data 'src/img/sprites.png:src/img' -F src/gui.py -n chess
+pyinstaller --add-data 'src/img/sprites.png:src/img' -F src/game.py -n chess
 rm -r build
